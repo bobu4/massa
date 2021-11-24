8df098f859160691a5a25c0baa2989650ddb950a
bobu4 <48527047+bobu4@users.noreply.github.com>
2021-11-24 17:24:53 +0200

    Update bal_tel.sh

--- bal_tel.sh
+++ bal_tel.sh
@@ -3,8 +3,8 @@ cd massa/massa-client
 active_rolls=$(./massa-client wallet_info | grep 'Active rolls' | cut -d\   -f3)
 if [ $active_rolls -gt "0" ]; then
 echo "More than 0"
-cd ; python3 tz.py "Attention!Your massa node hasn't active rolls on $(hostname)!" ; cd massa/massa-client
 elif [ $active_rolls -lt "1" ]; then
+cd ; python3 tz.py "Attention!Your massa node hasn't active rolls on $(hostname)!" ; cd massa/massa-client
 address=$(./massa-client wallet_info | grep 'Address' | cut -d\   -f2)
 ./massa-client buy_rolls $address 1 0
 fi
