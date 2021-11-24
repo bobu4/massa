3f388282515fa6f6b645f3ca167f7bca984d9a66
bobu4 <48527047+bobu4@users.noreply.github.com>
2021-11-24 16:39:43 +0200

    Create bal_tel.sh
    Updated monitoring script.

---
+++ bal_tel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd massa/massa-client
+active_rolls=$(./massa-client wallet_info | grep 'Active rolls' | cut -d\   -f3)
+if [ $active_rolls -gt "0" ]; then
+echo "More than 0"
+cd ; python3 tz.py "Attention!Your node hasn't active rolls!" ; cd massa/massa-client
+elif [ $active_rolls -lt "1" ]; then
+address=$(./massa-client wallet_info | grep 'Address' | cut -d\   -f2)
+./massa-client buy_rolls $address 1 0
+fi
