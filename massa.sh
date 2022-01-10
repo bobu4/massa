8b456e8cb81c7c125e49b35e8e76349f68f43874
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-01-10 18:02:34 +0200

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -54,7 +54,7 @@ sudo systemctl start massad
 cd massa/massa-client
 ./massa-client node_add_staking_private_keys $(./massa-client wallet_info | grep 'Private key' | cut -d\    -f3) ; ./massa-client node_get_staking_addresses
 read -p 'Enter discord id, obtained in massa bot: ' discord
-./massa-client node_testnet_rewards_program_ownership_proof $(./massa-client wallet_info | grep 'Address' | cut -d\   -f2) $discord
+signature=$(./massa-client node_testnet_rewards_program_ownership_proof $(./massa-client wallet_info | grep 'Address' | cut -d\   -f2) $discord)
 ./massa-client buy_rolls $(./massa-client wallet_info | grep 'Address' | cut -d\   -f2) 1 0
 ./massa-client wallet_info
 cd
@@ -65,3 +65,4 @@ install
 elif [ $choice -eq "2" ]; then
 update
 fi
+echo $signature
