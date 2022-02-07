42ee20fd2a0a044c3a66542afa7bb5afb5ad6768
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-02-07 19:23:24 +0200

    Update

--- auto.sh
+++ auto.sh
@@ -3,13 +3,21 @@ cd massa/massa-client
 staking_registered_address=$(./massa-client node_get_staking_addresses)
 staking_address=$(./massa-client wallet_info | grep 'Address' | cut -d\   -f2)
 candidate_rolls=$(./massa-client wallet_info | grep 'Candidate rolls' | cut -d\   -f3)
+warn_message_rolls=$(journalctl -u massad.service | grep -e "roll sale" --since "1 hour ago")
 if [ "$staking_registered_address" = "$staking_address" ]; then
 echo "Node was registered"
 else
 echo "Node wasn't registered"
 ./massa-client node_add_staking_private_keys $(./massa-client wallet_info | grep 'Private key' | cut -d\    -f3)
+fi
 if [ $candidate_rolls -gt "0" ]; then
 echo "More than 0"
 elif [ $candidate_rolls -lt "1" ]; then
 address=$(./massa-client wallet_info | grep 'Address' | cut -d\   -f2)
 ./massa-client buy_rolls $address 1 0
+fi
+if [[ $warn_message_rolls =~ "roll sale" -a $(echo "$(./massa-client wallet_info | grep 'Final balance' | cut -d\   -f3) > 99.99" | bc) -ne 0]]; then
+echo "Roll will be sell"
+address=$(./massa-client wallet_info | grep 'Address' | cut -d\   -f2)
+./massa-client buy_rolls $address 1 0
+fi
