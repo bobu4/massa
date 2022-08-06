80c8181cd048b4a38ec57eda4861e6b668c170c0
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-08-06 11:07:30 +0300

    Update clear_install.sh

--- clear_install.sh
+++ clear_install.sh
@@ -6,8 +6,8 @@ tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
 chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
 read -sp 'Enter the password for your massa wallet: ' passwd
-sed -i "/ massa_password=/d" $HOME/.bash_profile
-echo 'export massa_password="$passwd"' >> $HOME/.bash_profile
+sed -i "/ passwd=/d" $HOME/.bash_profile
+echo 'export passwd="$passwd"' >> $HOME/.bash_profile
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/multi_tool.sh) \
 -rb
 
@@ -19,7 +19,7 @@ After=network-online.target
 [Service]
 User=$USER
 WorkingDirectory=$HOME/massa/massa-node
-ExecStart=$HOME/massa/massa-node/massa-node -p "passwd"
+ExecStart=$HOME/massa/massa-node/massa-node -p "$passwd"
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
@@ -30,3 +30,11 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable massad
 sudo systemctl restart massad
+
+cd massa/massa-client
+./massa-client -p $passwd wallet_generate_secret_key
+./massa-client -p $passwd node_add_staking_secret_keys $(./massa-client -p $passwd wallet_info | grep 'Secret key' | cut -d\   -f3) ; ./massa-client -p $passwd node_get_staking_addresses
+read -p 'Enter discord id, obtained in massa bot: ' discord
+signature=$(./massa-client -p $passwd node_testnet_rewards_program_ownership_proof $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) $discord)
+./massa-client -p $passwd buy_rolls $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) 1 0
+echo $signature
