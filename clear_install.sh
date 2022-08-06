b90e85af0db38e34ede6047b321180451d4486e6
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-08-06 10:11:32 +0300

    Update clear_install.sh

--- clear_install.sh
+++ clear_install.sh
@@ -5,7 +5,7 @@ wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
 chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
-reap -sp 'Enter the password for your massa wallet: ' passwd
+read -sp 'Enter the password for your massa wallet: ' passwd
 sed -i "/ massa_password=/d" $HOME/.bash_profile
 echo 'export massa_password="$passwd"' >> $HOME/.bash_profile
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/multi_tool.sh) \
