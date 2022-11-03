48dcd35b47fde0c824e033cc568c3ff15f7485f0
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-11-03 13:37:22 +0300

    Update clear_install.sh

--- clear_install.sh
+++ clear_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
-wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.15.1/massa_TEST.15.1_release_linux.tar.gz
+wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.16.0/massa_TEST.16.0_release_linux.tar.gz
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
 chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
