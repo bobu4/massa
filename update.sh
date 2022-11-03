75fff86786c77299acb679f107b984683d7b7240
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-11-03 11:32:40 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+mkdir backup
+mv $HOME/massa/massa-node/config/node_privkey.key $HOME/backup/node_privkey.key
+mv $HOME/massa/massa-client/wallet.dat $HOME/backup/wallet.dat
+systemctl stop massad
+rm -rf massa
+sudo apt update && sudo apt upgrade -y
+sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
+wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.16.0/massa_TEST.16.0_release_linux.tar.gz
+tar -xzvf massa.tar.gz
+rm -rf massa.tar.gz
+chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
+read -sp 'Enter the password for your massa wallet: ' passwd
+sed -i "/ passwd=/d" $HOME/.bash_profile
+echo "export passwd=\"${passwd}\"" >> $HOME/.bash_profile
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/multi_tool.sh) \
+-rb
+mv $HOME/backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
+mv $HOME/backup/wallet.dat $HOME/massa/massa-client/wallet.dat 
+sudo systemctl restart massad
+
+sleep 10
+cd massa/massa-client
+
+./massa-client -p $passwd node_add_staking_secret_keys $(./massa-client -p $passwd wallet_info | grep 'Secret key' | cut -d\   -f3) ; ./massa-client -p $passwd node_get_staking_addresses
+read -p 'Enter discord id, obtained in massa bot: ' discord
+signature=$(./massa-client -p $passwd node_testnet_rewards_program_ownership_proof $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) $discord)
+./massa-client -p $passwd buy_rolls $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) 1 0
+echo $signature
