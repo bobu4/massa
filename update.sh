0cb4ce1969e1b8873789d9feea9573dd4cd4639b
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-12-05 11:36:49 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
 mkdir backup
-cp $HOME/massa/massa-node/config /root/backup/node_privkey.key
-cp $HOME/massa/massa-client /root/backup/wallet.dat
+cp $HOME/massa/massa-node/config/node_privkey.key /root/backup/node_privkey.key
+cp $HOME/massa/massa-client/wallet.dat /root/backup/wallet.dat
 systemctl stop massad
 rm -rf massa
 wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.17.1/massa_TEST.17.1_release_linux.tar.gz
@@ -13,30 +13,13 @@ chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
 read -sp 'Enter the password for your massa wallet: ' passwd
 sed -i "/ passwd=/d" $HOME/.bash_profile
 echo "export passwd=\"${passwd}\"" >> $HOME/.bash_profile
-
-cp /root/backup/node_privkey.key $HOME/massa/massa-node/config
-cp /root/backup/wallet.dat $HOME/massa/massa-client
-sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
-[Unit]
-Description=Massa Node
-After=network-online.target
-[Service]
-User=$USER
-WorkingDirectory=$HOME/massa/massa-node
-ExecStart=$HOME/massa/massa-node/massa-node -p "$passwd"
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target
-EOF
+cp /root/backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
+cp /root/backup/wallet.dat $HOME/massa/massa-client/wallet.dat
 sudo systemctl daemon-reload
 sudo systemctl enable massad
 sudo systemctl restart massad
-
 sleep 10
 cd massa/massa-client
-./massa-client -p $passwd wallet_generate_secret_key
 ./massa-client -p $passwd node_add_staking_secret_keys $(./massa-client -p $passwd wallet_info | grep 'Secret key' | cut -d\   -f3) ; ./massa-client -p $passwd node_get_staking_addresses
 read -p 'Enter discord id, obtained in massa bot: ' discord
 signature=$(./massa-client -p $passwd node_testnet_rewards_program_ownership_proof $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) $discord)
