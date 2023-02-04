fa15dbf532a7c7313a14a23e68881b78f303c899
bobu4 <48527047+bobu4@users.noreply.github.com>
2023-02-04 08:20:02 +0200

    Update clear_install.sh

--- clear_install.sh
+++ clear_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
+cd /mnt/data
 wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.19.1/massa_TEST.19.1_release_linux.tar.gz
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
