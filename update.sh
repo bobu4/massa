314edeabab94817d2cb8892d2d4b385ac0c5ec49
bobu4 <48527047+bobu4@users.noreply.github.com>
2023-02-12 16:56:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,19 +3,19 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
 rm -rf backup
 mkdir backup
-cp $HOME/massa/massa-node/config/node_privkey.key /root/backup/node_privkey.key
-cp $HOME/massa/massa-client/wallet.dat /root/backup/wallet.dat
+cp /mnt/data/massa/massa-node/config/node_privkey.key /mnt/data/backup/node_privkey.key
+cp /mnt/data/massa/massa-client/wallet.dat /mnt/data/backup/wallet.dat
 systemctl stop massad
 rm -rf massa
-wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.18.0/massa_TEST.18.0_release_linux.tar.gz
+wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.19.3/massa_TEST.19.3_release_linux.tar.gz
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
-chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
+chmod +x /mnt/data/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
 read -sp 'Enter the password for your massa wallet: ' passwd
 sed -i "/ passwd=/d" $HOME/.bash_profile
 echo "export passwd=\"${passwd}\"" >> $HOME/.bash_profile
-cp /root/backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
-cp /root/backup/wallet.dat $HOME/massa/massa-client/wallet.dat
+cp /mnt/data/backup/node_privkey.key /mnt/data/massa/massa-node/config/node_privkey.key
+cp /mnt/data/backup/wallet.dat /mnt/data/massa/massa-client/wallet.dat
 sudo systemctl daemon-reload
 sudo systemctl enable massad
 sudo systemctl restart massad
