ff44491078df5faf98d3256e9fc8354d198b49c6
bobu4 <48527047+bobu4@users.noreply.github.com>
2023-03-02 08:01:23 +0200

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -2,13 +2,14 @@
 function install {
 sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
-wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.1_1.1.0l-1~deb9u6_amd64.deb
-sudo dpkg -i libssl1.1_1.1.0l-1~deb9u6_amd64.deb
-wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.11.0/massa_TEST.11.0_release_linux.tar.gz
+cd path
+wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.20.0/massa_TEST.20.0_release_linux.tar.gz
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
-chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
-
+chmod +x $path/massa/massa-node/massa-node $path/massa/massa-client/massa-client
+read -sp 'Enter the password for your massa wallet: ' passwd
+sed -i "/ passwd=/d" $HOME/.bash_profile
+echo "export passwd=\"${passwd}\"" >> $HOME/.bash_profile
 sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
 [Unit]
 Description=Massa Node
@@ -27,22 +28,22 @@ sudo systemctl enable massad
 sudo systemctl daemon-reload
 sudo systemctl restart massad
 cd massa/massa-client
-./massa-client wallet_generate_private_key
-./massa-client node_add_staking_private_keys $(./massa-client wallet_info | grep 'Private key' | cut -d\    -f3) ; ./massa-client node_get_staking_addresses
+sleep 10
+./massa-client -p $passwd wallet_generate_secret_key
+./massa-client -p $passwd node_start_staking $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) ; ./massa-client -p $passwd node_get_staking_addresses
 read -p 'Enter discord id, obtained in massa bot: ' discord
-signature=$(./massa-client node_testnet_rewards_program_ownership_proof $(./massa-client wallet_info | grep 'Address' | cut -d\   -f2) $discord)
-./massa-client buy_rolls $(./massa-client wallet_info | grep 'Address' | cut -d\   -f2) 1 0
-./massa-client wallet_info
-cd
+signature=$(./massa-client -p $passwd node_testnet_rewards_program_ownership_proof $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) $discord)
+./massa-client -p $passwd buy_rolls $(./massa-client -p $passwd wallet_info | grep 'Address' | cut -d\   -f2) 1 0
 echo $signature
 }
 function update {
 systemctl stop massad
 sudo apt update && sudo apt upgrade -y
 sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
+cd path
 mkdir backup
-cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backup/node_privkey.key
-cp $HOME/massa/massa-client/wallet.dat $HOME/backup/wallet.dat
+cp $path/massa/massa-node/config/node_privkey.key $path/backup/node_privkey.key
+cp $path/massa/massa-client/wallet.dat $path/backup/wallet.dat
 rm -rf massa
 wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/TEST.11.3/massa_TEST.11.3_release_linux.tar.gz
 tar -xzvf massa.tar.gz
@@ -62,6 +63,7 @@ sleep 10
 cd
 #echo $signature
 }
+read -s 'Enter path to install massa: ' path
 read -p 'Enter 1 for clear install with backup or 2 to update: ' choice
 if [ $choice -eq "1" ]; then
 install
