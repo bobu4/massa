28bddbfa9c5b1db1485a58511a0a6d4f90280ea5
bobu4 <48527047+bobu4@users.noreply.github.com>
2024-01-15 19:39:15 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
 cd massa/massa-client
-staking_registered_address=$(./massa-client -p $passwd node_get_staking_addresses)
-staking_address=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.address')
-candidate_rolls=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.candidate_rolls')
-warn_message_rolls=$(journalctl -u massad.service --since "1 hour ago" | grep -e "roll sale")
-if [ "$staking_registered_address" = "$staking_address" ]; then
-echo "Node was registered"
+address=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.address')
+if [ $(echo "$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_balance') > 101" | bc) -ne 0 ]
+then
+echo "Buy one more roll"
+./massa-client -p $passwd buy_rolls $address 1 0
 else
-echo "Node wasn't registered"
-./massa-client -p $passwd node_add_staking_secret_keys $(./massa-client -p $passwd wallet_info -j | jq -r '.[].keypair.secret_key')
-fi
-if [ $candidate_rolls -gt "0" ]; then
-echo "More than 0"
-elif [ $candidate_rolls -lt "1" ]; then
-address=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.address')
-./massa-client -p $passwd buy_rolls $address 1 0
-fi
-if [[ $warn_message_rolls =~ "roll sale" ]]
-then
-if [ $(echo "$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_sequential_balance') > 99.99" | bc) -ne 0 ]
-then
-echo "Roll will be sell"
-address=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.address')
-./massa-client -p $passwd buy_rolls $address 1 0
-fi
+echo $(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_balance')
 fi
