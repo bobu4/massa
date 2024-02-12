565bf20fc7f3f587f9e68dc62b10daba9075d04d
bobu4 <48527047+bobu4@users.noreply.github.com>
2024-02-12 17:42:00 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,7 +4,10 @@ address=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.add
 if [ $(echo "$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_balance') > 101" | bc) -ne 0 ]
 then
 echo "Buy one more roll"
-./massa-client -p $passwd buy_rolls $address 1 0
+balance_tokens=$(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_balance')
+float_rolls=$(awk "BEGIN { printf(\"%.2f\", $balance_tokens / 100) }")
+rolls_to_buy=$(echo ${float_rolls%%.*})
+./massa-client -p $passwd buy_rolls $address $rolls_to_buy 0
 else
 echo $(./massa-client -p $passwd wallet_info -j | jq -r '.[].address_info.final_balance')
 fi
