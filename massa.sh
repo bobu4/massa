c0559f1397cfb42c8996a2950343f0da80f552dd
bobu4 <48527047+bobu4@users.noreply.github.com>
2024-02-13 09:41:36 +0200

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev cmake -y
-wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/MAIN.2.0/massa_MAIN.2.0_release_linux.tar.gz
+wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/MAIN.2.1/massa_MAIN.2.1_release_linux.tar.gz
 tar -xzvf massa.tar.gz
 rm -rf massa.tar.gz
 chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client
